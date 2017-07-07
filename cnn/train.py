ba8c39e42b4c94f82b306c02f6e0477229b5ccad
Gaurav Manek <gaurav@gauravmanek.com>
2017-07-07 14:25:08 +0800

    CNN code

--- cnn/train.py
+++ cnn/train.py
@@ -40,7 +40,7 @@ inc_global_step = tf.assign(global_step, global_step+1)
 train_op = tf.train.AdamOptimizer().minimize(loss)
 
 logger.info("Loading training supervisor...")
-sv = tf.train.Supervisor(logdir="train_logs/", global_step=global_step, summary_op=None, save_model_secs=600)
+sv = tf.train.Supervisor(logdir="cnn/train_logs/", global_step=global_step, summary_op=None, save_model_secs=600)
 logger.info("Done!")
 
 with sv.managed_session() as sess:
@@ -48,7 +48,7 @@ with sv.managed_session() as sess:
     batch = sess.run(global_step)
 
     # Set up tensorboard logging:
-    logwriter = tf.summary.FileWriter("train_logs/cifar-vgg/", sess.graph)
+    logwriter = tf.summary.FileWriter("cnn/train_logs/", sess.graph)
     logwriter.add_session_log(tf.SessionLog(status=tf.SessionLog.START), global_step=batch)
 
     logger.info("Starting training from batch {} to {}. Saving model every {}s.".format(batch, NUM_BATCHES, 600))
@@ -56,7 +56,7 @@ with sv.managed_session() as sess:
     while not sv.should_stop():
         if batch >= NUM_BATCHES:
             logger.info("Saving...")
-            sv.saver.save(sess, "train_logs/cifar-vgg/model.ckpt", global_step=batch)
+            sv.saver.save(sess, "cnn/train_logs/model.ckpt", global_step=batch)
             sv.stop()
             break
 
