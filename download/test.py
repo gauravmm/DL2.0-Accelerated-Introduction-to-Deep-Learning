df488f69525a9fbcbe7143dc567eddc97c0efd75
Gaurav Manek <gaurav@gauravmanek.com>
2017-07-18 18:15:38 +0800

    Support for YouTube8m dataset.

--- download/test.py
+++ download/test.py
@@ -2,9 +2,9 @@
 
 # You can just import whichever dataset you want directly:
 # Supported datasets are in the data package.
-from data import nodules, cifar10
+from data import nodules, cifar10, yt8m
 
-for mod in [nodules, cifar10]:
+for mod in [nodules, cifar10, yt8m]:
     print("Downloading: {}".format(mod.__name__))
     mod.get_train()
     mod.get_test()
