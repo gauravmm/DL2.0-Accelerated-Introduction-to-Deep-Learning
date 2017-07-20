a0f5fe8fff3ec6979411be2c009320cc909a186f
kingsleykuan <kingsley.kuan@gmail.com>
2017-07-20 10:28:19 +0800

    Add simple ssd and text cnn to pretrained weights batch download

--- workshop.py
+++ workshop.py
@@ -41,7 +41,7 @@ def run(args):
         if args.example:
             download_pretrained(args.example)
         else:
-            for example in ["cnn", "gan"]:
+            for example in ["cnn", "gan", "simple_ssd", "textcnn"]:
                 download_pretrained(example)
         has_effect = True
 
