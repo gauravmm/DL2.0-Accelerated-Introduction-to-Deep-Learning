f2d1921c4370e338deca5c3dcad8d994bb479424
Gaurav Manek <gaurav@gauravmanek.com>
2017-07-20 18:51:34 +0800

    Updated GANs and pushed pretrained.

--- gan/train.py
+++ gan/train.py
@@ -9,7 +9,7 @@ import logging
 logger = logging.getLogger("gan.train")
 
 # Create CIFAR-10 input
-BATCH_SIZE = 256
+BATCH_SIZE = 32
 data_generator = map((lambda inp: (inp[0]*2. - 1., inp[1])), utilities.infinite_generator(cifar10.get_train(), BATCH_SIZE))
 
 # Input images
@@ -49,15 +49,15 @@ generator_loss = tf.losses.sigmoid_cross_entropy(
 
 
 # Add summaries to visualise output images and losses
+summary_discriminator = tf.summary.merge([ \
+    tf.summary.scalar('summary/real_discriminator_loss', real_discriminator_loss), \
+    tf.summary.scalar('summary/fake_discriminator_loss', fake_discriminator_loss), \
+    tf.summary.scalar('summary/discriminator_loss', discriminator_loss)])
+
 generator_visualisation = tf.cast(((generator / 2.0) + 0.5) * 255.0, tf.uint8)
-tf.summary.image('summary/generator', generator_visualisation, max_outputs=8)
-
-tf.summary.scalar('summary/real_discriminator_loss', real_discriminator_loss)
-tf.summary.scalar('summary/fake_discriminator_loss', fake_discriminator_loss)
-tf.summary.scalar('summary/discriminator_loss', discriminator_loss)
-
-tf.summary.scalar('summary/generator_loss', generator_loss)
-
+summary_generator = tf.summary.merge([ \
+tf.summary.image('summary/generator', generator_visualisation, max_outputs=32), \
+tf.summary.scalar('summary/generator_loss', generator_loss)])
 
 # Get discriminator and generator variables to train separately
 discriminator_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
@@ -75,28 +75,37 @@ generator_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS,
 global_step = tf.Variable(0, name='global_step', trainable=False)
 
 with tf.control_dependencies(discriminator_update_ops):
-train_discriminator = tf.train. \
+    train_discriminator = tf.train. \
                           AdamOptimizer(learning_rate=0.0002, beta1=0.5). \
                           minimize(discriminator_loss,
                                    var_list=discriminator_variables)
 
 with tf.control_dependencies(generator_update_ops):
-train_generator = tf.train. \
+    train_generator = tf.train. \
                       AdamOptimizer(learning_rate=0.0002, beta1=0.5). \
                       minimize(generator_loss, global_step=global_step,
                                var_list=generator_variables)
 
 
+# We disable automatic summaries here, because the automatic system assumes that
+# any time you run any part of the graph, you will be providing values for _all_
+# summaries:
 sv = tf.train.Supervisor(logdir="gan/train_logs/", global_step=global_step,
-                         save_summaries_secs=10, save_model_secs=120)
+                         save_summaries_secs=None, save_model_secs=120)
 
 batch = 0
 with sv.managed_session() as sess:
+    # Set up tensorboard logging:
+    logwriter = tf.summary.FileWriter("gan/train_logs/", sess.graph)
+
     while not sv.should_stop():
         if batch > 0 and batch % 100 == 0:
-            logger.debug('Step {} of {}.'.format(batch, NUM_BATCHES))
+            logger.debug('Step {}.'.format(batch))
 
         inp, _ = next(data_generator)
-        sess.run(train_discriminator, feed_dict={n_input: inp})
-        sess.run(train_generator)
+        (_, sum_dis) = sess.run((train_discriminator, summary_discriminator), feed_dict={input_placeholder: inp})
+        logwriter.add_summary(sum_dis)
+        (_, sum_gen) = sess.run((train_generator, summary_generator))
+        logwriter.add_summary(sum_gen)
+
         batch += 1
